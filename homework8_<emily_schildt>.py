a2dfafb36d33ae746eaff8dee5e555a90d4be73c
eschildt <eschildt@uw.edu>
2026-02-21 05:34:41 +0000

    final

---
+++ homework8_<emily_schildt>.py
@@ -0,0 +1,9 @@
+pizza_orders = ['Pepperoni', 'Hawaiian', 'Cheese', 'Veggie', 'BBQ Chicken']
+finished_pizzas = []
+while pizza_orders:
+    current_pizza = pizza_orders.pop(0)
+    print(f"Your pizza pie is finished!")
+    finished_pizzas.append(current_pizza)
+
+for pizza in finished_pizzas:
+    print(f"The pizza {pizza} was made.")
